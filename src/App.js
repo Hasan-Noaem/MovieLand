4149eadf3ca05b3f79356bdbb9e3e109bdff9774
Hasan-Noaem <122913504+Hasan-Noaem@users.noreply.github.com>
2023-01-28 10:13:53 -0800

    ss

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./image/search.svg";
 import "./App.css";
 
-const API_URL = 'http://www.omdbapi.com/?apikey=95e77740'; 
+const API_URL = 'https://www.omdbapi.com/?apikey=95e77740'; 
 
 const App = () => {
     const [movies , setMovies] = useState([]);
